043ae76c44efe7344414c5d387d940353d32c825
Omale Arumona <omalearumona@gmail.com>
2024-05-13 13:58:28 +0100

    assignment completed

---
+++ server.js
@@ -0,0 +1,54 @@
+const { createServer } = require("node:http");
+const { readFileSync, writeFileSync } = require("fs");
+const url = require("url");
+
+const server = createServer((req, res) => {
+  const form = readFileSync("./index.html", "utf8");
+  if (req.url === "/" && req.method === "GET") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    res.end(form);
+  } else if (req.method === "GET") {
+    let data = url.parse(req.url, true);
+    let readData = readFileSync("./database.json", "utf8");
+
+    if (readData) {
+      let parsedData = JSON.parse(readData);
+      let database = [...parsedData, data.query];
+
+      let stringData = JSON.stringify(database);
+
+      writeFileSync("./database.json", stringData);
+    } else {
+      let database = [data.query];
+      let stringData = JSON.stringify(database);
+      writeFileSync("./database.json", stringData);
+    }
+
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    res.end(`<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+            </head>
+            <body>
+                <h3>Submitted successfully</h3>
+                <button type="button"><a href="http://localhost:8080">Back to homepage/form</a></button>
+            </body>
+            </html>
+    `);
+  } else {
+    res.end("Not Found");
+  }
+});
+
+const port = 8080;
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`);
+});
