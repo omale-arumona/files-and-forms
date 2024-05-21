c2545f195dfe7076ed4e0b4474eec1d542472d61
Omale Arumona <omalearumona@gmail.com>
2024-05-21 22:20:22 +0100

    added redirection

--- server.js
+++ server.js
@@ -20,27 +20,48 @@ const server = createServer((req, res) => {
       let stringData = JSON.stringify(database);
 
       writeFileSync("./database.json", stringData);
+
+      res.writeHead(201, {
+        "Content-Type": "text/plain",
+        // location: "/database",
+        "content-location": "/database",
+      });
+
+      let databaseRes = readFileSync("./database.json", "utf8");
+      console.log(databaseRes);
+      res.end(databaseRes);
     } else {
       let database = [data.query];
       let stringData = JSON.stringify(database);
       writeFileSync("./database.json", stringData);
+
+      res.writeHead(201, {
+        "Content-Type": "text/plain",
+        // location: "/database",
+        "content-location": "/database",
+      });
+
+      let databaseRes = readFileSync("./database.json", "utf8");
+      console.log(databaseRes);
+      res.end(databaseRes);
     }
-    res.writeHead(200, {
-      "Content-Type": "text/html",
-    });
-    res.end(`<!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Document</title>
-            </head>
-            <body>
-                <h3>Submitted successfully</h3>
-                <button type="button"><a href="http://localhost:8080">Back to homepage/form</a></button>
-            </body>
-            </html>
-    `);
+
+    // res.writeHead(200, {
+    //   "Content-Type": "text/html",
+    // });
+    // res.end(`<!DOCTYPE html>
+    //         <html lang="en">
+    //         <head>
+    //             <meta charset="UTF-8">
+    //             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //             <title>Document</title>
+    //         </head>
+    //         <body>
+    //             <h3>Submitted successfully</h3>
+    //             <button type="button"><a href="http://localhost:8080">Back to homepage/form</a></button>
+    //         </body>
+    //         </html>
+    // `);
   } else {
     res.writeHead(404, {
       "Content-Type": "text/plain",
